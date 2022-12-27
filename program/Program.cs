cccec12f19b2649c85020cf9ad099c31ceef39e4
Vika <tViks1506@mail.ru>
2022-12-27 13:47:47 +0300

    add program

---
+++ program/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите строку: ");
+string [] firstArray = Console.ReadLine().Split(" ");
+string [] dopArray = new string [firstArray.Length];
+int len = 3;
+int i=0;
+int j=0;
+
+while ( i < firstArray.Length)
+{
+    if (firstArray[i].Length <= len)
+    {
+        dopArray[j] = firstArray[i];
+        j++;
+    }
+    i++;
+}
+string [] secondArray = new string [j];
+for (int k = 0; k<j; k++)
+{
+    secondArray[k]= dopArray[k];
+}
+Console.WriteLine($"[{string.Join(", ",  secondArray)}]");
